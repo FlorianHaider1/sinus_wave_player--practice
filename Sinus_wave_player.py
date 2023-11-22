4b3ac0a75f70b232eaaca7bea4046275c18071ff
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-22 09:21:54 +0100

    Update Sinus_wave_player.py

---
+++ Sinus_wave_player.py
@@ -0,0 +1,9 @@
+# Music Player using sound card to play simple melodys.
+# Practice oop
+# Features:
+# - Notes insertable by calling it its 'natural' name, so C, D, E, etc.
+# - adding flat/sharp, by adding #/b to the Notes, maybe o for normal
+# - adding length of notes by adding a Number, i.e. 16, 8, 4, 2, 1
+# - adding pauses: P
+# - Notes then saved in tuples, (C,o,8)(P,o,1)(E,b,8) for (C Eightnote), (Full Pause), (Eb Eightnote) 
+# - Radiobutton for 60, 90, 120, 150 bpm, changing the length of notes accordingly
